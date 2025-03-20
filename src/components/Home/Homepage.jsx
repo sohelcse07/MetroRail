acffe4b0c330c531011ca9451e52b792d61538dd
RedwanAhmedTapuu <redwan25880@gmail.com>
2025-03-21 02:20:42 +0600

    dashboard added

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import HomeSection from '../HomeSection'
 import MetroTimeline from '../MetroTimeLine'
-import MetroRailChart from '../MetroRailChart'
 
 const Homepage = () => {
   return (
    <>
    <HomeSection/>
    <MetroTimeline/>
-   <MetroRailChart/>
    </>
   )
 }
